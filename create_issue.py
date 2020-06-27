269d85aac58b3ad06e42a17ca5c558c09e061205
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-27 11:47:10 +0300

    Update create_issue.py

--- create_issue.py
+++ create_issue.py
@@ -3,12 +3,22 @@ import os
 import aiohttp
 from gidgethub.aiohttp import GitHubAPI
 
+
 async def main():
     async with aiohttp.ClientSession() as session:
         gh = GitHubAPI(
             session,
-            "mariatta",
+            "mezgoodle",
             oauth_token=os.getenv("GH_AUTH")
         )
+        response = await gh.post(
+            '/repos/mezgoodle/hello-github-actions/issues',
+            data={
+                'title': 'We got a problems',
+                'body': 'Use more emojies!',
+            }
+        )
+        print(f"Issue created at {response['html_url']}")
+
 
 asyncio.run(main())
