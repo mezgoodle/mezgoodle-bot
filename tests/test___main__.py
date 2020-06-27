89cb69de02b15b73d0cc63b79545c0fe3a99c261
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-27 18:35:24 +0300

    Try to fix tests problems

--- tests/test___main__.py
+++ tests/test___main__.py
@@ -1,38 +1,38 @@
-from aiohttp import web
+from aiohttp import web, client
 import pytest
 
 from webservice import __main__ as main
 
 
-async def test_ping(aiohttp_client):
+async def test_ping():
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await aiohttp_client(app)
+    client_ = await client(app)
     headers = {"x-github-event": "ping",
                "x-github-delivery": "1234"}
     data = {"zen": "testing is good"}
-    response = await client.post("/", headers=headers, json=data)
+    response = await client_.post("/", headers=headers, json=data)
     assert response.status == 200
 
 
-async def test_success(aiohttp_client):
+async def test_success():
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await aiohttp_client(app)
+    client_ = await client(app)
     headers = {"x-github-event": "project",
                "x-github-delivery": "1234"}
     # Sending a payload that shouldn't trigger any networking, but no errors
     # either.
     data = {"action": "created"}
-    response = await client.post("/", headers=headers, json=data)
+    response = await client_.post("/", headers=headers, json=data)
     assert response.status == 200
 
 
-async def test_failure(aiohttp_client):
+async def test_failure():
     """Even in the face of an exception, the server should not crash."""
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await aiohttp_client(app)
+    client_ = await client(app)
     # Missing key headers.
-    response = await client.post("/", headers={})
+    response = await client_.post("/", headers={})
     assert response.status == 500
