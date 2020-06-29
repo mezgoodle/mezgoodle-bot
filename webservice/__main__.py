d9b7084c20b2e39f1879e2cbff4009e061a4081e
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-29 09:56:28 +0300

    Update __main__.py

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -116,44 +116,46 @@ async def pr_opened(event, gh, *args, **kwargs):
 
 @router.register("pull_request", action="closed")
 @router.register("pull_request", action="labeled")
+@router.register("pull_request", action="merged")
 async def backport_pr(event, gh, *args, **kwargs):
-    if event.data["pull_request"]["merged"]:
+    print(event.data)
+    # if event.data["pull_request"]["merged"]:
 
-        merged_by = event.data["pull_request"]["merged_by"]["login"]
-        created_by = event.data["pull_request"]["user"]["login"]
-        issue_comment_url = event.data["pull_request"]["issue_url"]
+    #     merged_by = event.data["pull_request"]["merged_by"]["login"]
+    #     created_by = event.data["pull_request"]["user"]["login"]
+    #     issue_comment_url = event.data["pull_request"]["issue_url"]
 
-        pr_labels = []
-        if event.data["action"] == "labeled":
-            pr_labels = [event.data["label"]]
-        else:
-            gh_issue = await gh.getitem(
-                event.data["repository"]["issues_url"],
-                {"number": f"{event.data['pull_request']['number']}"},
-            )
-            pr_labels = await gh.getitem(gh_issue["labels_url"])
+    #     pr_labels = []
+    #     if event.data["action"] == "labeled":
+    #         pr_labels = [event.data["label"]]
+    #     else:
+    #         gh_issue = await gh.getitem(
+    #             event.data["repository"]["issues_url"],
+    #             {"number": f"{event.data['pull_request']['number']}"},
+    #         )
+    #         pr_labels = await gh.getitem(gh_issue["labels_url"])
 
-        branches = [
-            label["name"].split()[-1]
-            for label in pr_labels
-            if label["name"].startswith("needs backport to")
-        ]
+    #     branches = [
+    #         label["name"].split()[-1]
+    #         for label in pr_labels
+    #         if label["name"].startswith("needs backport to")
+    #     ]
 
-        if branches:
-            easter_egg = ""
-            if random.random() < 0.1:
-                easter_egg = EASTER_EGG
-            thanks_to = ""
-            if created_by == merged_by or merged_by == "mezgoodle-bot":
-                thanks_to = f"Thanks @{created_by} for the PR 🌮🎉."
-            else:
-                thanks_to = f"Thanks @{created_by} for the PR, and @{merged_by} for merging it 🌮🎉."
-            message = (
-                f"{thanks_to}. I'm working now to backport this PR to: {', '.join(branches)}."
-                f"\n🐍🍒⛏🤖 {easter_egg}"
-            )
+    #     if branches:
+    #         easter_egg = ""
+    #         if random.random() < 0.1:
+    #             easter_egg = EASTER_EGG
+    #         thanks_to = ""
+    #         if created_by == merged_by or merged_by == "mezgoodle-bot":
+    #             thanks_to = f"Thanks @{created_by} for the PR 🌮🎉."
+    #         else:
+    #             thanks_to = f"Thanks @{created_by} for the PR, and @{merged_by} for merging it 🌮🎉."
+    #         message = (
+    #             f"{thanks_to}. I'm working now to backport this PR to: {', '.join(branches)}."
+    #             f"\n🐍🍒⛏🤖 {easter_egg}"
+    #         )
 
-            await leave_comment(gh, issue_comment_url, message)
+    #         await leave_comment(gh, issue_comment_url, message)
 
 
 async def leave_comment(gh, issue_comment_url, message):
