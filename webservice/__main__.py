305fd2c4740a170dec72d87bfa977d19040d7272
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-06-29 09:44:11 +0300

    Try to create function

--- webservice/__main__.py
+++ webservice/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import sys
 import traceback
+import random
 
 
 import aiohttp
@@ -14,6 +15,7 @@ from gidgethub import apps
 
 router = routing.Router()
 cache = cachetools.LRUCache(maxsize=500)
+EASTER_EGG = "I'm not a robot! I'm not a robot!"
 
 routes = web.RouteTableDef()
 
@@ -110,6 +112,81 @@ async def pr_opened(event, gh, *args, **kwargs):
         oauth_token=installation_access_token["token"],
     )
     print(response)
+
+
+@router.register("pull_request", action="closed")
+@router.register("pull_request", action="labeled")
+async def backport_pr(event, gh, *args, **kwargs):
+    if event.data["pull_request"]["merged"]:
+
+        issue_number = event.data["pull_request"]["number"]
+        merged_by = event.data["pull_request"]["merged_by"]["login"]
+        created_by = event.data["pull_request"]["user"]["login"]
+        issue_comment_url = event.data["pull_request"]["issue_url"]
+
+        commit_hash = event.data["pull_request"]["merge_commit_sha"]
+
+        pr_labels = []
+        if event.data["action"] == "labeled":
+            pr_labels = [event.data["label"]]
+        else:
+            gh_issue = await gh.getitem(
+                event.data["repository"]["issues_url"],
+                {"number": f"{event.data['pull_request']['number']}"},
+            )
+            pr_labels = await gh.getitem(gh_issue["labels_url"])
+
+        branches = [
+            label["name"].split()[-1]
+            for label in pr_labels
+            if label["name"].startswith("needs backport to")
+        ]
+
+        if branches:
+            easter_egg = ""
+            if random.random() < 0.1:
+                easter_egg = EASTER_EGG
+            thanks_to = ""
+            if created_by == merged_by or merged_by == "mezgoodle-bot":
+                thanks_to = f"Thanks @{created_by} for the PR 🌮🎉."
+            else:
+                thanks_to = f"Thanks @{created_by} for the PR, and @{merged_by} for merging it 🌮🎉."
+            message = (
+                f"{thanks_to}. I'm working now to backport this PR to: {', '.join(branches)}."
+                f"\n🐍🍒⛏🤖 {easter_egg}"
+            )
+
+            await util.leave_comment(gh, issue_number, message)
+
+            sorted_branches = sorted(
+                branches, reverse=True, key=lambda v: tuple(map(int, v.split(".")))
+            )
+
+            for branch in sorted_branches:
+                await kickoff_backport_task(
+                    gh, commit_hash, branch, issue_number, created_by, merged_by, issue_comment_url
+                )
+
+
+async def kickoff_backport_task(
+    gh, commit_hash, branch, issue_number, created_by, merged_by, issue_comment_url
+):
+    try:
+        tasks.backport_task.delay(
+            commit_hash,
+            branch,
+            issue_number=issue_number,
+            created_by=created_by,
+            merged_by=merged_by,
+        )
+    except (redis_ex.ConnectionError, kombu_ex.OperationalError) as ex:
+        err_message = f"I'm having trouble backporting to `{branch}`. Reason: '`{ex}`'. Please retry by removing and re-adding the `needs backport to {branch}` label."
+        await util.leave_comment(gh, issue_comment_url, err_message)
+
+
+async def leave_comment(gh, issue_comment_url, message):
+    data = {"body": message}
+    await gh.post(f'{issue_comment_url}/comments', data=data)
 
 
 @router.register("issue_comment", action="created")
