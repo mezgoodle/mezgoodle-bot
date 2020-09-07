68f5ca3cb119bf654470bde7784cf8f22a99c9a9
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-07 17:43:45 +0000

    #16 First attempt for testing

---
+++ tests/test__main__.py
@@ -0,0 +1,15 @@
+from aiohttp import web
+import pytest
+
+from webservice import __main__ as main
+
+
+async def test_ping(aiohttp_client):
+    app = web.Application()
+    app.router.add_post("/", main.main)
+    client = await aiohttp_client(app)
+    headers = {"x-github-event": "ping",
+               "x-github-delivery": "1234"}
+    data = {"zen": "testing is good"}
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 200
