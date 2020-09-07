a2acbedf220ff33319b6dc95717e5814c320183d
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-07 09:25:57 +0300

    #13 Test email function

--- webservice/consts.py
+++ webservice/consts.py
@@ -1,3 +1,12 @@
 # File with constants for project
+
+# For events
 issue_type = 'issue'
 pr_type = 'pull_request'
+
+# For tests
+test_title = 'Test title'
+test_sender = 'mezgoodle'
+test_sender_url = 'mezgoodle.com'
+test_event_url = 'issue.com'
+test_body = 'Test body'
--- webservice/tests.py
+++ webservice/tests.py
@@ -1,5 +1,17 @@
+from .mail import send_mail
+from .consts import *
+
 def test():
-    print('Hello')
+    try:
+        send_mail(issue_type,
+            test_title,
+            test_sender,
+            test_sender_url,
+            test_event_url,
+            test_body
+        )
+    except BaseException:
+        print('Okay')
 
 if __name__ == '__main__':
     test()
