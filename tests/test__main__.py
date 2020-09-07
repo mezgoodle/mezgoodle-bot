f4187491f41fc232311380bd35aa540948c02a3a
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-07 18:08:23 +0000

    #16 Add one more test for main function

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -1,5 +1,4 @@
 from aiohttp import web
-# import pytest
 
 from webservice import __main__ as main
 
@@ -11,5 +10,18 @@ async def test_ping(aiohttp_client):
     headers = {"x-github-event": "ping",
                "x-github-delivery": "1234"}
     data = {"zen": "testing is good"}
+    response = await client.post("/", headers=headers, json=data)
+    assert response.status == 200
+
+
+async def test_success(aiohttp_client):
+    app = web.Application()
+    app.router.add_post("/", main.webhook)
+    client = await aiohttp_client(app)
+    headers = {"x-github-event": "project",
+               "x-github-delivery": "1234"}
+    # Sending a payload that shouldn't trigger any networking, but no errors
+    # either.
+    data = {"action": "created"}
     response = await client.post("/", headers=headers, json=data)
     assert response.status == 200
