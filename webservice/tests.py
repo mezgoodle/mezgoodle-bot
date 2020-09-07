fd59ceaa1dd49a5a9c94cba9eb5477b1f2417c4a
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-07 09:30:42 +0300

    #13 autopep8 update

--- webservice/tests.py
+++ webservice/tests.py
@@ -5,14 +5,15 @@ from consts import *
 def test():
     try:
         send_mail(issue_type,
-            test_title,
-            test_sender,
-            test_sender_url,
-            test_event_url,
-            test_body
-        )
+                  test_title,
+                  test_sender,
+                  test_sender_url,
+                  test_event_url,
+                  test_body
+                  )
     except BaseException:
         print('Okay')
+
 
 if __name__ == '__main__':
     test()
