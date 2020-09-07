db36aa3ac68b0d968abc08af9a9b274b2315d26a
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-07 17:49:09 +0000

    #16 Import client from aiohttp

--- tests/test__main__.py
+++ tests/test__main__.py
@@ -1,13 +1,13 @@
-from aiohttp import web
+from aiohttp import web, client
 import pytest
 
 from webservice import __main__ as main
 
 
-async def test_ping(aiohttp_client):
+async def test_ping(client):
     app = web.Application()
     app.router.add_post("/", main.main)
-    client = await aiohttp_client(app)
+    client = await client(app)
     headers = {"x-github-event": "ping",
                "x-github-delivery": "1234"}
     data = {"zen": "testing is good"}
