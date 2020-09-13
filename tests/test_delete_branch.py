2e1040a9d173c5fecdb353cd26c6922326ce69cd
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-13 17:26:55 +0000

    #16 Try to test deleting

---
+++ tests/test_delete_branch.py
@@ -0,0 +1,33 @@
+from gidgethub import sansio
+
+from webservice import pr
+
+
+class FakeGH:
+    def __init__(self):
+        self.post_data = None
+
+    async def delete(self, url):
+        self.delete_url = url
+
+
+async def test_branch_deleted_when_pr_merged():
+    data = {
+        "action": "closed",
+        "pull_request": {
+            "number": 5722,
+            "user": {"login": "mezgoodle-bot"},
+            "merged": True,
+            "merged_by": {"login": "mezgoodle-bot"},
+            "head": {"ref": "backport-17ab8f0-3.7"},
+        },
+    }
+    event = sansio.Event(data, event="pull_request", delivery_id="1")
+
+    gh = FakeGH()
+    await pr.events_pr.router.dispatch(event, gh)
+    assert gh.post_data is None  # does not leave a comment
+    assert (
+        gh.delete_url
+        == f"/repos/mezgoodle/mezgoodle-bot/git/refs/heads/{data['pull_request']['head']['ref']}"
+    )
