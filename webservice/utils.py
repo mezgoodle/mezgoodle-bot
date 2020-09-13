6590a17ab0cc95c7edbbadad5b530f572a486c18
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-13 17:56:43 +0000

    #16 Output data

--- webservice/utils.py
+++ webservice/utils.py
@@ -3,6 +3,7 @@ from .config import PRIVATE_KEY, APP_ID
 
 
 async def get_info(event, gh):
+    print(event.data)
     installation_id = event.data["installation"]["id"]
     installation_access_token = await apps.get_installation_access_token(
         gh,
