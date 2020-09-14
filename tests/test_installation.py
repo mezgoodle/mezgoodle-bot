d6e1084d57941ce8242c9b6fe5c64750d1b1c3f0
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-14 21:52:27 +0300

    Some updates

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -17,11 +17,11 @@ class FakeGH:
 
 async def test_installation():
 	"""Test installation feedback"""
-    data = consts.TEST_DATA_INSTALLATION
-    event = sansio.Event(data, event='installation', delivery_id='1')
+	data = consts.TEST_DATA_INSTALLATION
+	event = sansio.Event(data, event='installation', delivery_id='1')
 
-    gh = FakeGH()
-    await installation.router.dispatch(event, gh)
-    assert (
-        gh.post_url == f'/repos/{consts.BOT_NAME}/issues'
-    )
+	gh = FakeGH()
+	await installation.router.dispatch(event, gh)
+	assert (
+	    gh.post_url == f'/repos/{consts.BOT_NAME}/issues'
+	)
--- tests/test_issues.py
+++ tests/test_issues.py
@@ -17,11 +17,11 @@ class FakeGH:
 
 async def test_issue_opened():
 	"""Test issue comment creation"""
-    data = consts.TEST_DATA_ISSUES
-    event = sansio.Event(data, event='issue_comment', delivery_id='1')
+	data = consts.TEST_DATA_ISSUES
+	event = sansio.Event(data, event='issue_comment', delivery_id='1')
 
-    gh = FakeGH()
-    await issues.router.dispatch(event, gh)
-    assert (
-        gh.post_url == f'{consts.ISSUE_URL}/reactions'
-    )
+	gh = FakeGH()
+	await issues.router.dispatch(event, gh)
+	assert (
+	    gh.post_url == f'{consts.ISSUE_URL}/reactions'
+	)
