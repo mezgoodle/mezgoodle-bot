1d95ed9bf6d83ae0c6c290025bc330f7b48175de
Maxim Zavalniuk <mezgoodle@gmail.com>
2020-09-14 21:58:12 +0300

    Some updates

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -6,22 +6,22 @@ from webservice import installation, consts
 
 
 class FakeGH:
-	"""Create fake GitHub object"""
-	def __init__(self):
-		self.post_data = None
+    """Create fake GitHub object"""
+    def __init__(self):
+	    self.post_data = None
 
-	async def post(self, url):
-		"""Trigger post method"""
-		self.post_url = url
+    async def post(self, url):
+	    """Trigger post method"""
+	    self.post_url = url
 
 
 async def test_installation():
-	"""Test installation feedback"""
-	data = consts.TEST_DATA_INSTALLATION
-	event = sansio.Event(data, event='installation', delivery_id='1')
+    """Test installation feedback"""
+    data = consts.TEST_DATA_INSTALLATION
+    event = sansio.Event(data, event='installation', delivery_id='1')
 
-	gh = FakeGH()
-	await installation.router.dispatch(event, gh)
-	assert (
-	    gh.post_url == f'/repos/{consts.BOT_NAME}/issues'
-	)
+    gh = FakeGH()
+    await installation.router.dispatch(event, gh)
+    assert (
+        gh.post_url == f'/repos/{consts.BOT_NAME}/issues'
+    )
